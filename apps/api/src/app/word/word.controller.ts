d6a8727bfd5dd0090483776df91ceb7bf7609fa9
Alexander Bashenin <timebender00@gmail.com>
2025-02-12 22:39:43 +0400

    [BE] added getRandomWord method

--- apps/api/src/app/word/word.controller.ts
+++ apps/api/src/app/word/word.controller.ts
@@ -10,51 +10,63 @@ import { DEFAULT_WORDS_PER_PAGE } from '../common/constants';
 
 @Controller('words/word')
 export class WordController {
-	constructor(private readonly wordService: WordService) {}
+    constructor(private readonly wordService: WordService) {}
 
-	@Post()
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: WordDto })
-	@Roles(Role.ADMIN)
-	async create(@Body() createWordDto: CreateWordDto): Promise<Word> {
-		return await this.wordService.createWord(createWordDto);
-	}
+    @Post()
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto })
+    @Roles(Role.ADMIN)
+    async create(@Body() createWordDto: CreateWordDto): Promise<Word> {
+        return await this.wordService.createWord(createWordDto);
+    }
 
-	@Get()
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: WordDto, isArray: true })
-	@Roles(Role.ADMIN)
-	async findPagedWords(@Query('page') page: string, @Query('perPage') perPage: string): Promise<PageDto<Word>> {
-		return await this.wordService.findWords({ where: {}, orderBy: {}, page: +page || 1, perPage: +perPage || DEFAULT_WORDS_PER_PAGE });
-	}
+    @Get()
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto, isArray: true })
+    @Roles(Role.ADMIN)
+    async findPagedWords(@Query('page') page: string, @Query('perPage') perPage: string): Promise<PageDto<Word>> {
+        return await this.wordService.findWords({
+            where: {},
+            orderBy: {},
+            page: +page || 1,
+            perPage: +perPage || DEFAULT_WORDS_PER_PAGE,
+        });
+    }
 
-	@Get(':id/update-image')
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: ImageDto })
-	async findOneAndUpdateImage(@Param('id') id: string): Promise<Image> {
-		return await this.wordService.updateWordImage(+id);
-	}
+	@Get('random')
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto })
+    async getRandomWord(): Promise<Word> {
+        return await this.wordService.getRandomWord();
+    }
 
-	@Get(':id')
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: WordDto })
-	async findOne(@Param('id') id: string): Promise<Word> {
-		return await this.wordService.findWordById(+id);
-	}
+    @Get(':id/update-image')
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: ImageDto })
+    async findOneAndUpdateImage(@Param('id') id: string): Promise<Image> {
+        return await this.wordService.updateWordImage(+id);
+    }
 
-	@Put(':id')
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: WordDto })
-	@Roles(Role.ADMIN)
-	async update(@Param('id') id: string, @Body() updateWordDto: UpdateWordDto): Promise<Word> {
-		return await this.wordService.updateWord(+id, updateWordDto);
-	}
+    @Get(':id')
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto })
+    async findOne(@Param('id') id: string): Promise<Word> {
+        return await this.wordService.findWordById(+id);
+    }
 
-	@Delete(':id')
-	@ApiBearerAuth('jwt')
-	@ApiResponse({ status: HttpStatus.OK, type: WordDto })
-	@Roles(Role.ADMIN)
-	async remove(@Param('id') id: string): Promise<Word> {
-		return await this.wordService.blockWord(+id);
-	}
+    @Put(':id')
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto })
+    @Roles(Role.ADMIN)
+    async update(@Param('id') id: string, @Body() updateWordDto: UpdateWordDto): Promise<Word> {
+        return await this.wordService.updateWord(+id, updateWordDto);
+    }
+
+    @Delete(':id')
+    @ApiBearerAuth('jwt')
+    @ApiResponse({ status: HttpStatus.OK, type: WordDto })
+    @Roles(Role.ADMIN)
+    async remove(@Param('id') id: string): Promise<Word> {
+        return await this.wordService.blockWord(+id);
+    }
 }
--- apps/api/src/app/word/word.service.ts
+++ apps/api/src/app/word/word.service.ts
@@ -9,103 +9,135 @@ import { ImageService } from './image/image.service';
 import { UnsplashService } from './image/unsplash/unsplash.service';
 import { PageDto } from '../common/dto/page.dto';
 
-
 @Injectable()
 export class WordService {
-	constructor(
-		private prisma: PrismaService,
-		private imageService: ImageService,
-		private unsplashService: UnsplashService,
-	) {}
+    constructor(
+        private prisma: PrismaService,
+        private imageService: ImageService,
+        private unsplashService: UnsplashService,
+    ) {}
 
-	async createWord(data: CreateWordDto): Promise<Word> {
-		return await this.prisma.word.create({ data });
-	}
+    async createWord(data: CreateWordDto): Promise<Word> {
+        return await this.prisma.word.create({ data });
+    }
 
-	async findWords({
-		where,
-		orderBy,
-		page = 1,
-		perPage = DEFAULT_WORDS_PER_PAGE,
-	  }: {
-		where?: Prisma.WordWhereInput;
-		orderBy?: Prisma.WordOrderByWithRelationInput;
-		page?: number;
-		perPage?: number;
-	  }): Promise<PageDto<Word>> {
-		const skip = (page - 1) * perPage;
-		const take = perPage;
-		const result = await this.prisma.word.findMany({
-		  where: { ...where, isVisible: true },
-		  orderBy: { id: 'asc', ...orderBy },
-		  skip,
-		  take,
+    async findWords({
+        where,
+        orderBy,
+        page = 1,
+        perPage = DEFAULT_WORDS_PER_PAGE,
+    }: {
+        where?: Prisma.WordWhereInput;
+        orderBy?: Prisma.WordOrderByWithRelationInput;
+        page?: number;
+        perPage?: number;
+    }): Promise<PageDto<Word>> {
+        const skip = (page - 1) * perPage;
+        const take = perPage;
+        const result = await this.prisma.word.findMany({
+            where: { ...where, isVisible: true },
+            orderBy: { id: 'asc', ...orderBy },
+            skip,
+            take,
+        });
+
+        return {
+            data: result,
+            meta: {
+                total: await this.prisma.word.count({
+                    where: { ...where, isVisible: true },
+                }),
+                perPage,
+                lastPage: Math.ceil(
+                    (await this.prisma.word.count({
+                        where: { ...where, isVisible: true },
+                    })) / perPage,
+                ),
+                currentPage: page,
+                prev: page > 1 ? page - 1 : null,
+                next:
+                    page <
+                    Math.ceil(
+                        (await this.prisma.word.count({
+                            where: { ...where, isVisible: true },
+                        })) / perPage,
+                    )
+                        ? page + 1
+                        : null,
+            },
+        };
+    }
+
+    async findWordById(id: number): Promise<Word> {
+        const word = await this.prisma.word.findUnique({
+            where: { id, isVisible: true },
+            include: { defaultImage: true },
+        });
+        if (!word) {
+            throw new NotFoundException(`Word with id ${id} not found`);
+        }
+        const usages = await this.prisma.wordUsage.findMany({
+            where: { wordId: word.id, isVerified: true },
+        });
+        const result = {
+            ...word,
+            usages,
+        };
+        return result;
+    }
+
+    async getRandomWord(): Promise<Word> {
+		const randomId = await this.prisma.word.count({
+			where: { isVisible: true, defaultImageId: { not: null } },
 		});
+        const randomWord = await this.prisma.word.findFirst({
+			where: { isVisible: true, defaultImageId: { not: null } },
+			include: { defaultImage: true },
+            skip: Math.floor(Math.random() * randomId),
+        });
+        return randomWord;
+    }
 
-		return {
-		  data: result,
-		  meta: {
-			total: await this.prisma.word.count({ where: { ...where, isVisible: true } }),
-			perPage,
-			lastPage: Math.ceil((await this.prisma.word.count({ where: { ...where, isVisible: true } })) / perPage),
-			currentPage: page,
-			prev: page > 1 ? page - 1 : null,
-			next: page < Math.ceil((await this.prisma.word.count({ where: { ...where, isVisible: true } })) / perPage) ? page + 1 : null,
-		  }
-		}
-	  }
+    async updateWord(id: number, data: UpdateWordDto): Promise<Word> {
+        return await this.prisma.word.update({
+            where: { id },
+            data,
+        });
+    }
 
-	async findWordById(id: number): Promise<Word> {
-		const word = await this.prisma.word.findUnique({ where: { id, isVisible: true }, include: { defaultImage: true } });
-		if (!word) {
-			throw new NotFoundException(`Word with id ${id} not found`);
-		}
-		const usages = await this.prisma.wordUsage.findMany({ where: { wordId: word.id, isVerified: true } });
-		const result = {
-			...word,
-			usages,
-		};
-		return result;
-	}
+    async blockWord(id: number): Promise<Word> {
+        return await this.prisma.word.update({
+            where: { id },
+            data: { isVisible: false },
+        });
+    }
 
-	async updateWord(id: number, data: UpdateWordDto): Promise<Word> {
-		return await this.prisma.word.update({
-			where: { id },
-			data,
-		});
-	}
+    async updateWordImage(id: number): Promise<Image> {
+        const word = await this.prisma.word.findUnique({
+            where: { id, isVisible: true },
+        });
+        if (!word) {
+            throw new NotFoundException(`Word with id ${id} not found`);
+        }
+        const context = word.context ? word.word + ' ' + word.context : word.word;
+        const photos = await this.unsplashService.searchPhotos(context);
+        const image = this.convertPhotosToDefaultImage(photos!);
+        const result = await this.imageService.create(image);
+        await this.updateWord(id, { defaultImageId: result.id });
+        return result;
+    }
 
-	async blockWord(id: number): Promise<Word> {
-		return await this.prisma.word.update({
-			where: { id },
-			data: { isVisible: false },
-		});
-	}
-
-	async updateWordImage(id: number): Promise<Image> {
-		const word = await this.prisma.word.findUnique({ where: { id, isVisible: true } });
-		if (!word) {
-			throw new NotFoundException(`Word with id ${id} not found`);
-		}
-		const context = word.context ? word.word + ' ' + word.context : word.word;
-		const photos = await this.unsplashService.searchPhotos(context);
-		const image = this.convertPhotosToDefaultImage(photos!);
-		const result = await this.imageService.create(image);
-		await this.updateWord(id, { defaultImageId: result.id });
-		return result;
-	}
-
-	private convertPhotosToDefaultImage(photo: Photos): CreateImageDto {
-		const image = photo.results[0];
-		return {
-			url: image.urls.regular,
-			urls: image.urls,
-			provider: 'unsplash',
-			externalId: image.id,
-			authorLink: image.user.links.html,
-			authorName: image.user.name,
-			blurHash: image.blur_hash,
-			description: image.description,
-		};
-	}
+    private convertPhotosToDefaultImage(photo: Photos): CreateImageDto {
+        const image = photo.results[0];
+        return {
+            url: image.urls.regular,
+            urls: image.urls,
+            provider: 'unsplash',
+            externalId: image.id,
+            authorLink: image.user.links.html,
+            authorName: image.user.name,
+            blurHash: image.blur_hash,
+            description: image.description,
+        };
+    }
 }
