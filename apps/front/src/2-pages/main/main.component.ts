90752e406aff3c821dab1280f50260891e43d2ab
Alexander Bashenin <timebender00@gmail.com>
2025-02-13 11:01:22 +0400

    [FE] fixed ling on main page

--- apps/front/src/2-pages/main/main.component.ts
+++ apps/front/src/2-pages/main/main.component.ts
@@ -5,10 +5,19 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { LayoutComponent } from '../layout';
 import { WordRandomComponent } from '@/widgets/word-random/word-random.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-main',
-    imports: [CommonModule, LayoutComponent, MatButtonModule, MatDividerModule, MatIconModule, WordRandomComponent],
+    imports: [
+        CommonModule,
+        LayoutComponent,
+        MatButtonModule,
+        MatDividerModule,
+        MatIconModule,
+        WordRandomComponent,
+        RouterLink,
+    ],
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
